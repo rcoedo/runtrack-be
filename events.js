da06d84acadff6d4a3d44fdb0359018d97f1a5f2
Daniel V. Brañas <daniel.branas@trabesoluciones.net>
2018-06-12 23:06:23 +0200

    Update events API

--- events.js
+++ events.js
@@ -1,18 +1,100 @@
 const events = {
-  1: {
-    id: 1,
-    name: "Carrera 1",
+  219: {
+    id: 219,
+    name: "I kilometer of solidarity AC",
+    description: "Kilometer of solidarity is a demo race to show Runtrack capabilities. Enjoy it!",
     joined: false,
     coordinates: { lat: 43.365963, long: -8.404712 },
     date: 1537696800165,
     endDate: 1537714800165,
-    distance: 10,
-    imageUri: null,
+    distance: 1.6,
+    users: 0,
+    isInternal: true,
+    route: [
+        {
+                latitude: 43.3767151,
+                longitude: -8.4234881,
+        },
+        {
+                latitude: 43.3766059,
+                longitude: -8.4222865,
+        },
+        {
+                latitude: 43.3760912,
+                longitude: -8.4212995,
+        },
+        {
+                latitude: 43.3748435,
+                longitude: -8.4203553,
+        },
+        {
+                latitude: 43.3736269,
+                longitude: -8.4198403,
+        },
+        {
+                latitude: 43.3730498,
+                longitude: -8.4191537,
+        },
+        {
+                latitude: 43.3721295,
+                longitude: -8.4190679,
+        },
+        {
+                latitude: 43.3715056,
+                longitude: -8.4187889,
+        },
+        {
+                latitude: 43.3711001,
+                longitude: -8.4183383,
+        },
+        {
+                latitude: 43.3705541,
+                longitude: -8.4168577,
+        },
+        {
+                latitude: 43.370133,
+                longitude: -8.4157848,
+        },
+        {
+                latitude: 43.3696494,
+                longitude: -8.4152484,
+        },
+        {
+                latitude: 43.3684639,
+                longitude: -8.4138536,
+        },
+        {
+                latitude: 43.3681832,
+                longitude: -8.4131455,
+        },
+        {
+                latitude: 43.3680584,
+                longitude: -8.4118366,
+        },
+        {
+                latitude: 43.36823,
+                longitude: -8.4107208,
+        },
+        {
+                latitude: 43.3684327,
+                longitude: -8.4094119,
+        },
+        {
+                latitude: 43.3690879,
+                longitude: -8.4074378,
+        },
+        {
+                latitude: 43.3698679,
+                longitude: -8.4057212,
+        },
+    ],
+    imageUri: 1,
     status: "UNKNOWN",
     unit: "km",
+    prize: null,
   },
-  2: {
-    id: 2,
+  180: {
+    id: 180,
     name: "Carrera 2",
     joined: false,
     coordinates: { lat: 43.370027, long: -8.415713 },
@@ -20,16 +102,18 @@ const events = {
     endDate: 1537714800165,
     distance: 10,
     imageUri: null,
+    route: [],
     status: "UNKNOWN",
     unit: "km",
   },
-  3: {
-    id: 3,
+  190: {
+    id: 190,
     name: "Carrera 3",
     joined: false,
     coordinates: { lat: 43.343308, long: -8.405715 },
     date: 1537696800165,
     endDate: 1537714800165,
+    route: [],
     distance: 10,
     imageUri: null,
     status: "UNKNOWN",
--- index.js
+++ index.js
@@ -7,4 +7,25 @@ app.get("/events", (req, res) => {
   res.send(Object.entries(events).map(([k, v]) => v));
 });
 
+app.get("/events/:eventId", (req, res) => {
+  if (events[req.params.eventId]) {
+    res.send(events[req.params.eventId]);
+  } else {
+    res.status(404);
+    res.send({});
+  }
+});
+
+app.post("/events/:eventId/join", (req, res) => {
+  if (events[req.params.eventId]) {
+    events[req.params.eventId].users += 1;
+    res.status(200);
+    res.send(events[req.params.eventId]);
+  } else {
+    res.status(404);
+    res.send({});
+  }
+
+});
+
 app.listen(process.env.PORT || 3000, () => console.log("runtrack backend listening on port 3000"));
